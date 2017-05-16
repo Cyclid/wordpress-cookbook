4608f71765b5ce4400021c87f3268744f2b1f2ee
Kristian Van Der Vliet <vanders@liqwyd.com>
2017-05-16 19:06:10 +0100

    Let the wrapper cookbook manage the nginx configuration

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -40,23 +40,3 @@ node.normal['nginx']['default_site_enabled'] = false
 include_recipe "chef_nginx"
 
 include_recipe "wordpress::app"
-
-template "#{node['nginx']['dir']}/sites-enabled/wordpress.conf" do
-  source "nginx.conf.erb"
-  variables(
-    :docroot          => node['wordpress']['dir'],
-    :server_name      => node['wordpress']['server_name'],
-    :server_aliases   => node['wordpress']['server_aliases'],
-    :server_port      => node['wordpress']['server_port']
-  )
-  action :create
-end
-
-# The following block is specifically for OS's like CentOS that include a
-# default site as a part of the install. This block will only be triggered if
-# node['nginx']['default_site_enable'] is set to false.
-file File.join(node['nginx']['dir'], 'conf.d', 'default.conf') do
-  action :delete
-  notifies :reload, 'service[nginx]'
-  only_if { !node['nginx']['default_site_enabled'] }
-end
